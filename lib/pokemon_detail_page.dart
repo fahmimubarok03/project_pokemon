bc5c1a352678823e77b6183898f355e2921e3ee6
fahmimubarok03 <m.mubarok23@student.uisi.ac.id>
2025-12-15 20:55:07 +0700

    mengedit bagian class _PokemonDetailPageState (Scaffold, SafeArea, Padding, Column)

--- lib/pokemon_detail_page.dart
+++ lib/pokemon_detail_page.dart
@@ -3,7 +3,7 @@ import 'package:pertemuan_10/model/pokemon.dart';
 
 class PokemonDetailPage extends StatefulWidget {
   final Pokemon pokemon;
-  
+
   const PokemonDetailPage({Key? key, required this.pokemon}) : super(key: key);
 
   @override
@@ -13,6 +13,23 @@ class PokemonDetailPage extends StatefulWidget {
 class _PokemonDetailPageState extends State<PokemonDetailPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
