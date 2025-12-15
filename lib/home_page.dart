340f4d83fdf8d84645e98880d4b0568e768ccc2a
fahmimubarok03 <m.mubarok23@student.uisi.ac.id>
2025-12-15 21:35:15 +0700

    edit bagian pokemon_card_widget dan home_page

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -3,109 +3,103 @@ import 'package:pertemuan_10/model/dummy_data.dart';
 import 'package:pertemuan_10/pokemon_detail_page.dart';
 import 'package:pertemuan_10/shared/widget/pokemon_card_widget.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     final searchName = TextEditingController();
     return Scaffold(
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              headerText(),
-              SizedBox(height: 20),
-              SearchBar(searchName),
-              SizedBox(height: 20),
-              listPokemon(),
-            ],
-          ),
-        ),
+        child: Padding(padding: const EdgeInsets.all(24.0), child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            headerText(),
+            SizedBox(height: 20),
+            searchBar(searchName),
+            SizedBox(height: 20),
+            listPokemon(),
+          ],
+        )),
       ),
     );
   }
 
-  Widget SearchBar(TextEditingController searchName) {
-    return Row(
-      spacing: 10,
-      children: [
-        Expanded(
-          child: TextFormField(
-            controller: searchName,
-            decoration: InputDecoration(
-              hintText: 'Search Pokemon',
-              border: OutlineInputBorder(),
-              prefixIcon: Icon(Icons.search),
-            ),
-          ),
-        ),
-        Container(
-          padding: EdgeInsets.all(8.0),
-          height: 60,
-          width: 60,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(8),
-            color: Colors.grey,
-          ),
-          child: Icon(Icons.filter_list),
-        ),
-      ],
-    );
-  }
-
-  Widget headerText() {
+  Widget headerText(){
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Text(
-          'Pokedex',
-          style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
-        ),
-        SizedBox(height: 8),
-        Text(
-          'Search for a Pokemon by name or using its National Pokedex number.',
-          style: TextStyle(fontSize: 16, color: Colors.grey),
+              'Pokedex',
+              style: TextStyle(
+                fontSize:35,
+                fontWeight: FontWeight.bold
+              ),
+            ),
+            Text(
+              'Search for a pokemon by name or using its National Pokedex number.',
+              style: TextStyle(
+                fontSize:20,
+              ),
+            ),
+      ],
+    );
+  }
+
+  Widget searchBar(TextEditingController searchName){
+    return Row(
+      spacing: 10,
+      children: [
+        Expanded(child: TextFormField(
+          controller: searchName,
+          decoration: InputDecoration(
+            hintText: "Search a Pokemon", 
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(12)
+            ),
+            prefixIcon: Icon(Icons.search),
+            
+          ),
+        )),
+        Container(
+          padding: EdgeInsets.all(8.0),
+          height: 55,
+          width: 55,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: const Color.fromRGBO(93, 95, 124, 1),
+          ),
+          child: Icon(Icons.filter_list, color: Colors.white),
         ),
       ],
     );
   }
 
-  Widget listPokemon() {
+  Widget listPokemon (){
     return Expanded(
       child: GridView.builder(
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 3 / 4,
-          crossAxisSpacing: 12,
-          mainAxisSpacing: 12,
-        ),
+        crossAxisCount: 2,
+        childAspectRatio: 3/4,
+        crossAxisSpacing: 12,
+        mainAxisSpacing: 12
+        ), 
         itemCount: dummyPokemonList.length,
         itemBuilder: (context, index) {
+          final pokemon = dummyPokemonList[index];
           return PokemonCardWidget(
             onTap: () {
               Navigator.push(
                 context,
                 MaterialPageRoute(
-                  builder: (context) =>
-                      PokemonDetailPage(pokemon: dummyPokemonList[index]),
-                ),
+                  builder: (context) => PokemonDetailPage(pokemon: pokemon))
               );
             },
-
-            imageUrl: dummyPokemonList[index].imageUrl,
-            name: dummyPokemonList[index].name,
-            type: dummyPokemonList[index].type,
+            imageUrl: pokemon.imageUrl,
+            name: pokemon.name,
+            type: pokemon.type,
           );
-        },
-      ),
+        },),
     );
   }
 }
--- lib/shared/widget/pokemon_card_widget.dart
+++ lib/shared/widget/pokemon_card_widget.dart
@@ -2,65 +2,55 @@ import 'package:flutter/material.dart';
 
 class PokemonCardWidget extends StatelessWidget {
   final String imageUrl;
-  final String name;
+  final String? name;
   final String type;
   final VoidCallback? onTap;
+  final bool showType;
 
   const PokemonCardWidget({
     Key? key,
     required this.imageUrl,
-    required this.name,
+    this.name,
     required this.type,
-    required this.onTap,
+    this.onTap,
+    this.showType = true
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      color: Colors.transparent,
-      child: InkWell(
-        borderRadius: BorderRadius.circular(12),
-        onTap: onTap,
-        child: Container(
-          decoration: BoxDecoration(
-            color: _getTypeColor(type),
-            borderRadius: BorderRadius.circular(12),
-          ),
-          padding: const EdgeInsets.all(8),
-          child: Column(
-            children: [
-              Expanded(
-                child: Image.network(
-                  imageUrl,
-                  fit: BoxFit.contain,
-                ),
-              ),
-              const SizedBox(height: 8),
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        decoration: BoxDecoration(
+          color: type == "Grass"
+              ? const Color.fromRGBO(196, 228, 213, 1)
+              : type == "Fire"
+              ? const Color.fromRGBO(239, 221, 186, 1)
+              : const Color.fromRGBO(190, 219, 221, 1),
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Column(
+          children: [
+            Image.network(
+              imageUrl, 
+              fit: BoxFit.fill, 
+              width: double.infinity
+            ),
+
+            if (name != null)
               Text(
-                name,
-                style: const TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
+                name!,
+                style: TextStyle(
+                  fontSize: 16, 
+                  fontWeight: FontWeight.bold
                 ),
               ),
+              
+            if (showType)
               Text(type),
-            ],
-          ),
+          ],
         ),
       ),
     );
-  }
-
-  Color _getTypeColor(String type) {
-    switch (type) {
-      case 'Grass':
-        return const Color.fromRGBO(196, 228, 213, 1);
-      case 'Fire':
-        return const Color.fromRGBO(239, 221, 186, 1);
-      case 'Water':
-        return const Color.fromRGBO(190, 219, 221, 1);
-      default:
-        return Colors.grey.shade300;
-    }
   }
 }
